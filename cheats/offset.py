c65fb0b17861644b782af6a61d6d9a735cf37df8
mmxx <75475886+Retmon403@users.noreply.github.com>
2023-12-18 14:20:20 +0800

    Add files via upload

---
+++ cheats/offset.py
@@ -0,0 +1,13 @@
+
+class Offset:
+    dwEntityList = 0x17c18e0
+    dwLocalPlayerController = 0x1810ee0
+    dwViewMatrix = 0x1820100
+    dwViewAngles = 0x16dabe8
+    dwViewAngles_Add = 0x6140
+    dwForceAttack = 0x16c1e32
+    dwForceJump = 0x16c2342
+
+
+if __name__ == '__main__':
+    pass
---
+++ cheats/ui_menu.py
@@ -0,0 +1,20 @@
+from threading import Lock
+import win32con
+
+
+class UI_MENU:
+
+    esp_box = True
+    esp_bons = True
+    aim_bot = True
+    aim_scope = 180
+    aim_rcs = True
+    aim_speed = 150
+    vk_items = [win32con.VK_MENU, win32con.VK_LBUTTON,
+                win32con.VK_RBUTTON]
+    aim_key = 0
+    key_select = 0
+
+    bhop = True
+    bhop_delay_start = 15
+    bhop_delay_end = 18
